0f2e5a0f1217bd74982749172ba6fa31cbfcac02
SuhakMV <sugamish@gmail.com>
2022-11-17 18:40:55 +0200

    17111840

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "pages/Home";
+import { Movies } from "pages/Movies";
+import { MovieDetails } from "pages/MovieDetails";
+import { Cast } from "pages/Cast";
+import { Reviews } from "pages/Reviews";
+
+import { NotFound } from "pages/NotFound";
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+    </Container>
   );
 };
